f997a85a2c0b7e014c96e39b7fb249629a260f9f
Oliveiraflavia <flalima041015@gmail.com>
2023-06-05 09:21:15 -0300

    Inicio controle de versão

---
+++ src/main/java/com/itb/inf2hm/comercio/model/Produto.java
@@ -0,0 +1,16 @@
+package com.itb.inf2hm.comercio.model;
+
+public class Produto {
+
+	
+	private long id;
+	private String nome;
+	private String codigoBarras;
+	private double preco;
+	
+	
+	// public  : Acesso liberado para todas as classes
+	// private : Propriedade (atributo ou método) só pode ser acessado 
+	//           dentro da própria classe
+	
+}
