15a96660c4475902705a330b9c73f920c3bf4577
Flvia Oliveira <flalima041015@gmail.com>
2023-06-13 11:49:29 -0300

    Ajustes no controller Loja

--- src/main/java/com/itb/inf2hm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2hm/comercio/controller/LojaController.java
@@ -13,18 +13,36 @@ import com.itb.inf2hm.comercio.model.Produto;
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
-  List<Produto> produtos = new ArrayList<Produto>();
+	
+  List<Produto> listaDeProdutos = new ArrayList<Produto>();
 	
 	
 	@GetMapping("/listar")
-	public String listarProdutos(Model model){
+	public String listarProdutos(Model model) {
 		
 		Produto produto = new Produto();
+		produto.setId(145l);
 	    produto.setNome("Televisor 70' Samsung");
 	    produto.setCodigoBarras("JFALJFDOE2514");
 	    produto.setPreco(6541.25);
 	    
+	    Produto produto2 = new Produto();
+	    produto2.setId(146l);
+	    produto2.setNome("Máquina de Lavar Brastemp");
+	    produto.setCodigoBarras("JFELJFDOA2910");
+	    produto2.setPreco(4321.54);
 		
+	    //Adicionando os produtos no Arraylist
+	    listaDeProdutos.add(produto);
+	    listaDeProdutos.add(produto2);
+	    
+	    model.addAttribute("produtos", listaDeProdutos);
 		return "produtos";
 	}
 	
+	@GetMapping("/novo-produto")
+	
+	public String novoProduto () {
+		return "novo-produto";
+	}
+}
--- src/main/java/com/itb/inf2hm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2hm/comercio/model/Produto.java
@@ -7,10 +7,35 @@ public class Produto {
 	private String nome;
 	private String codigoBarras;
 	private double preco;
-	
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+
+}
+
 	
 	// public  : Acesso liberado para todas as classes
 	// private : Propriedade (atributo ou método) só pode ser acessado 
 	//           dentro da própria classe
 	
-}
